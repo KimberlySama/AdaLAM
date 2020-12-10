fb4c8340f42ea16603920d94e94e11c845d877e9
Xingyan Wang <xingyan_wang1@brown.edu>
2020-12-09 23:34:00 -0500

    Update compare.py

--- Comparison/compare.py
+++ Comparison/compare.py
@@ -1,33 +1,58 @@
-class Points:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-
-file1 = open('Left_TP.txt', 'r') 
-count = 0
-
-set_TP = set()
-while True: 
-    count += 1
+import numpy as np
+import cv2 as cv
+# file1 = open('Left_TP.txt', 'r') 
+# count = 0
+# while True: 
+#     count += 1
   
-    # Get next line from file 
-    line = file1.readline().strip()
-    line = line.replace(",", "")
+#     # Get next line from file 
+#     line = file1.readline() 
     
-    # pnt = Points(x,y)
-    # print(splitLine)
-    if line!="" :
-        splitLine = line.split()
-        pnt = Points()
-        set_TP.add(pnt)
-
-    if not line: 
-        break
-    # print("Line{}: {}".format(count, line.strip())) 
-print("Lines number is: ", count-1)
-print("set_TP size is: ", len(set_TP))
-
-
+#     # if line is empty 
+#     # end of file is reached 
+#     if not line: 
+#         break
+#     print("Line{}: {}".format(count, line.strip())) 
+# print("Lines number is: ", count-1)
   
-file1.close()
+# file1.close()
+
+def read_data(file_path):
+    f = open(file_path, 'r')
+    data = []
+    for line in f:
+        data.append([float(coord) for coord in line.split(', ')])
+    return np.array(data)
+
+k1 = read_data("Left_TP.txt")
+k2 = read_data("Right_TP.txt")
+
+F, mask = cv.findFundamentalMat(k1,k2,cv.FM_8POINT)
+pts1 = k1[mask.ravel()==1]
+pts2 = k2[mask.ravel()==1]
+out1 = k1[mask.ravel()==0]
+out2 = k2[mask.ravel()==0]
+
+
+
+# adalam = read_data("Adalam_matched.txt")
+# true_pos = read_data("Left_TP.txt")
+# true_neg = read_data("Left_TN.txt")
+# threshold = 0.2
+# n_tp = 0
+# n_tn = 0
+
+
+
+# for coord in adalam:
+#     for tp_coord in true_pos:
+#         np.linalg.norm(np.array(coord)-np.array(tp_coord))
+#         if np.linalg.norm(np.array(coord)-np.array(tp_coord)) < threshold:
+#             n_tp += 1
+#             break
+        
+#     for tp_coord in true_neg:
+#         np.linalg.norm(np.array(coord)-np.array(tp_coord))
+#         if np.linalg.norm(np.array(coord)-np.array(tp_coord)) < threshold:
+#             n_tn += 1
+#             break
